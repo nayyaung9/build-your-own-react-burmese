d650633770c269919b030477c2076c114189775c
nayyaung9 <naygit.developer@gmail.com>
2021-08-13 17:25:16 +0630

    init examples folder

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,5 +1,3 @@
-var REACT_TEXT_ELEMENT = 1;
-
 function createElement(type, props, ...children) {
   return {
     type,
@@ -14,7 +12,7 @@ function createElement(type, props, ...children) {
 
 function createTextElement(text) {
   return {
-    type: REACT_TEXT_ELEMENT,
+    type: "TEXT_ELEMENT",
     props: {
       nodeValue: text,
       children: [],
@@ -24,37 +22,17 @@ function createTextElement(text) {
 
 function render(element, container) {
   const dom =
-    element.type == REACT_TEXT_ELEMENT
+    element.type == "TEXT_ELEMENT"
       ? document.createTextNode("")
       : document.createElement(element.type);
-
   const isProperty = (key) => key !== "children";
-
   Object.keys(element.props)
     .filter(isProperty)
     .forEach((name) => {
       dom[name] = element.props[name];
     });
   element.props.children.forEach((child) => render(child, dom));
-
   container.appendChild(dom);
-}
-
-let nextUnitOfWork = null;
-
-function workLoop(deadline) {
-  let shouldYield = false;
-  while (nextUnitOfWork && !shouldYield) {
-    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
-    shouldYield = deadline.timeRemaining() < 1;
-  }
-  requestIdleCallback(workLoop);
-}
-
-requestIdleCallback(workLoop);
-
-function performUnitOfWork(nextUnitOfWork) {
-  // TODO
 }
 
 const Didact = {
@@ -64,9 +42,11 @@ const Didact = {
 
 /** @jsx Didact.createElement */
 const element = (
-  <div id="foo">
-    <a>bar</a>
-    <b />
+  <div style="background: salmon">
+    <h1>Hello World</h1>
+    <h2>
+      From How to build your react in Myanmar Language
+    </h2>
   </div>
 );
 
